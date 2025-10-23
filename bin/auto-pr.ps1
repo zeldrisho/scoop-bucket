f742d01ba6352aa64b7ebce11585c14e690bf4c5
Zeldris <0trims.vehicle@icloud.com>
2025-10-24 00:26:17 +0700

    restore to default

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "zeldrisho/scoop-bucket:master"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
